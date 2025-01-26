87668c4d5078bfd32c1f548594daf60b874c24e5
Balbazar <erdiozbek78@gmail.com>
2025-01-26 18:32:10 +0100

    Home page design was simplify.

--- composeBase/src/main/java/com/oyetech/composebase/projectQuotesFeature/homeScreen/QuotesHomeScreenSetup.kt
+++ composeBase/src/main/java/com/oyetech/composebase/projectQuotesFeature/homeScreen/QuotesHomeScreenSetup.kt
@@ -1,45 +1,27 @@
 package com.oyetech.composebase.projectQuotesFeature.homeScreen
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.oyetech.composebase.base.BaseScaffold
 import com.oyetech.composebase.projectQuotesFeature.views.toolbar.QuoteToolbarActionItems
 import com.oyetech.composebase.projectQuotesFeature.views.toolbar.QuoteToolbarEvent
 import com.oyetech.composebase.projectQuotesFeature.views.toolbar.QuoteToolbarSetup
 import com.oyetech.composebase.projectRadioFeature.navigationRoutes.RadioAppProjectRoutes
-import com.oyetech.composebase.projectRadioFeature.screens.languageList.LanguageListScreenSetup
 import com.oyetech.composebase.sharedScreens.quotes.listScreen.QuoteListScreenSetup
 import com.oyetech.models.quotes.enums.QuoteListEnum
-import com.oyetech.models.quotes.enums.QuoteListEnum.All
-import com.oyetech.models.quotes.enums.QuoteListEnum.Random
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
-import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
-import timber.log.Timber
 
 /**
 Created by Erdi Özbek
@@ -87,7 +69,7 @@ fun QuotesHomeScreenSetup(
                 })
         }) { padding ->
         Column(modifier = Modifier.padding(padding)) {
-            Spacer(modifier = Modifier.height(8.dp))
+//            Spacer(modifier = Modifier.height(8.dp))
 
             QuotesHomeView(
                 pagerState = pagerState,
@@ -113,60 +95,61 @@ private fun QuotesHomeView(
 
     Column(Modifier.fillMaxSize()) {
         // Scrollable Tab Row
-        ScrollableTabRow(
-            selectedTabIndex = pagerState.currentPage,
-            modifier = Modifier.fillMaxWidth(),
-            edgePadding = 0.dp
-        ) {
-            tabEnums.forEachIndexed { index, _ ->
-                Tab(
-                    selected = pagerState.currentPage == index,
-                    onClick = {
-                        coroutineScope.launch {
-                            pagerState.animateScrollToPage(index)
-                        }
-                    }
-                ) {
-                    Row(
-                        modifier = Modifier.padding(8.dp),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = tabNameList[index],
-                            style = MaterialTheme.typography.labelLarge,
-                            color = if (pagerState.currentPage == index) {
-                                MaterialTheme.colorScheme.primary
-                            } else {
-                                MaterialTheme.colorScheme.onBackground
-                            }
-                        )
-                    }
-                }
-            }
-        }
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier.weight(1f),
-            key = { tabEnums[it] }
-        ) { page ->
-            Timber.d("page == $page")
-            key(tabEnums[page]) {
-                when (tabEnums[page]) {
-                    All -> {
-                        QuoteListScreenSetup(navigationRoute)
-                        Timber.d("Quotes")
-                    }
-
-                    Random -> {
-                        LanguageListScreenSetup(navigationRoute)
-                        Timber.d("Languages")
-                    }
-                }
-
-            }
-        }
+//        ScrollableTabRow(
+//            selectedTabIndex = 0,
+//            modifier = Modifier.fillMaxWidth(),
+//            edgePadding = 0.dp
+//        ) {
+//            tabEnums.forEachIndexed { index, _ ->
+//                Tab(
+//                    selected = pagerState.currentPage == index,
+//                    onClick = {
+//                        coroutineScope.launch {
+//                            pagerState.animateScrollToPage(index)
+//                        }
+//                    }
+//                ) {
+//                    Row(
+//                        modifier = Modifier.padding(8.dp),
+//                        horizontalArrangement = Arrangement.Center,
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
+//                        Text(
+//                            text = tabNameList[index],
+//                            style = MaterialTheme.typography.labelLarge,
+//                            color = if (pagerState.currentPage == index) {
+//                                MaterialTheme.colorScheme.primary
+//                            } else {
+//                                MaterialTheme.colorScheme.onBackground
+//                            }
+//                        )
+//                    }
+//                }
+//            }
+//        }
+//        HorizontalPager(
+//            state = pagerState,
+//            modifier = Modifier.weight(1f),
+//            key = { tabEnums[it] }
+//        ) { page ->
+//            Timber.d("page == $page")
+//            key(tabEnums[page]) {
+//                when (tabEnums[page]) {
+//                    All -> {
+//                        QuoteListScreenSetup(navigationRoute)
+//                        Timber.d("Quotes")
+//                    }
+//
+//                    Random -> {
+//                        LanguageListScreenSetup(navigationRoute)
+//                        Timber.d("Languages")
+//                    }
+//                }
+//
+//            }
+        QuoteListScreenSetup(navigationRoute)
     }
+//    }
 }
 
 @Composable
