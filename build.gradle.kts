0e3eb7b24a4af1b4dc62a3d0e4f513eb78c8fb2a
Balbazar <erdiozbek78@gmail.com>
2025-09-23 00:25:43 +0200

    Toml version was added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     extra.apply {
-        set("kotlinVersion", "2.0.20")
+        set("kotlinVersion", libs.versions.kotlin)
     }
 
     dependencies {
@@ -9,7 +9,7 @@ buildscript {
 //         classpath("com.android.tools:r8:8.5.10")
 
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.nav}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+        classpath(libs.kotlin.gradle.plugin)
         classpath("org.jetbrains.kotlinx:kover:0.5.0")
         classpath("com.google.gms:google-services:4.4.2")
         classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
@@ -25,9 +25,9 @@ ksp {
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20" apply false
 
 
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
@@ -35,7 +35,7 @@ plugins {
     id("io.gitlab.arturbosch.detekt").version("1.20.0-RC2")
     id("com.android.test") version "8.7.0" apply false
     id("androidx.baselineprofile") version "1.2.4" apply false
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
 
 }
 apply(plugin = "kover")
--- diModule/build.gradle.kts
+++ diModule/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
 }
 
 android {
