885108fc51f617ac52888b67fa425541401c59d9
Balbazar <erdiozbek78@gmail.com>
2025-09-22 23:34:58 +0200

    Libraries was refactored.

--- composeBase/build.gradle.kts
+++ composeBase/build.gradle.kts
@@ -27,51 +27,42 @@ android {
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation(platform("io.insert-koin:koin-bom:3.5.0"))
 
-    implementation(AndroidLibraries.coreKtx)
-
+    implementation("androidx.core:core-ktx:1.2.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation(Libraries.lifecycleRuntimeKtx)
-    implementation(Libraries.activityCompose)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.media:media:1.7.1")
 
-    // Compose dependencies
-    implementation(platform(Libraries.composeBom))
-    implementation(Libraries.composeUi)
-    implementation("androidx.paging:paging-compose:3.3.5")
-    implementation(Libraries.composeUiGraphics)
-    implementation(Libraries.composeUiToolingPreview)
-    implementation(project(":subImpl:tools"))
-    debugImplementation(Libraries.composeUiTooling)
-    debugImplementation(Libraries.composeUiTestManifest)
-    implementation(Libraries.material3)
-
-
-
-    implementation(project(Modules.domain))
-    implementation(project(Modules.glideModule))
-    implementation(project(Modules.model))
-    implementation(project(Modules.radioService))
-    implementation(project(Modules.languageModule))
-    implementation(project(":subImpl:denemeModule"))
-
-    implementation(Libraries.timber)
-    api(Libraries.koin)
-
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
-
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.runtime:runtime:1.7.7")
-
-    implementation("androidx.navigation:navigation-compose:2.8.6")
     implementation("androidx.compose.foundation:foundation:1.7.7")
+    implementation("androidx.paging:paging-compose:3.3.5")
+    implementation("androidx.navigation:navigation-compose:2.8.6")
     implementation("androidx.navigation:navigation-runtime-ktx:2.8.6")
-    implementation(Libraries.koinCompose)
-    implementation(project.dependencies.platform("io.insert-koin:koin-bom:${Versions.koin}"))
+
     implementation("io.insert-koin:koin-core")
     implementation("io.insert-koin:koin-androidx-compose")
     implementation("io.insert-koin:koin-androidx-compose-navigation")
 
     implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3") // En güncel sürümü kullanabilirsin
-    implementation("androidx.media:media:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+    implementation(project(":subImpl:tools"))
+    implementation(project(":subImpl:denemeModule"))
+    implementation(project(Modules.domain))
+    implementation(project(Modules.glideModule))
+    implementation(project(Modules.model))
+    implementation(project(Modules.radioService))
+    implementation(project(Modules.languageModule))
+
+
 
 }
