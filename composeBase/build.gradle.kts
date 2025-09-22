53d914d124ad655188764581d9365f825c601f48
Balbazar <erdiozbek78@gmail.com>
2025-09-23 01:08:43 +0200

    Compose base build gradle transformation was done.

--- composeBase/build.gradle.kts
+++ composeBase/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("org.jetbrains.kotlin.plugin.compose")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("org.jetbrains.kotlin.plugin.parcelize")
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
