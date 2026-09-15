e9d523b1fb64e198f908e9c9a6314b66d7e79bd7
Balbazar <erdiozbek78@gmail.com>
2026-09-15 20:37:10 +0200

    Some file was added.

--- kmpFeatures/src/commonMain/kotlin/com/oyetech/kmpfeatures/KmpFeaturesApp.kt
+++ kmpFeatures/src/commonMain/kotlin/com/oyetech/kmpfeatures/KmpFeaturesApp.kt
@@ -5,22 +5,23 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.oyetech.kmpfeatures.di.KmpFeaturesKoin
-import com.oyetech.kmpfeatures.navigation.kmpFeaturesNavGraph
 import com.oyetech.kmpfeatures.navigation.KmpFeaturesRoutes
-import org.koin.compose.KoinApplication
+import com.oyetech.kmpfeatures.navigation.kmpFeaturesNavGraph
+import org.koin.dsl.koinConfiguration
 
 @Composable
 fun KmpFeaturesApp() {
-    KoinApplication(application = { modules(KmpFeaturesKoin.module) }) {
-        val navController = rememberNavController()
+    koinConfiguration { modules(KmpFeaturesKoin.module) }
 
-        MaterialTheme {
-            NavHost(
-                navController = navController,
-                startDestination = KmpFeaturesRoutes.Home,
-            ) {
-                kmpFeaturesNavGraph(navController)
-            }
+
+    val navController = rememberNavController()
+
+    MaterialTheme {
+        NavHost(
+            navController = navController,
+            startDestination = KmpFeaturesRoutes.Home,
+        ) {
+            kmpFeaturesNavGraph(navController)
         }
     }
 }
